1044511c7e337e938e9f978044d43aecc02737d3
bunyamingoymen <bunyamingoymen@tutanota.com>
2021-02-03 17:45:56 +0300

    Some changes were made in the project according to the changes in the database

---
+++ src/entity/kullanici_degerlendirmesi.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author bgoym
+ */
+public class kullanici_degerlendirmesi {
+    private int kullanici_degerlendirme_id;
+    private int user_id;
+    private int film_id;
+    private int degerlendirme;
+
+    public kullanici_degerlendirmesi() {
+    }
+
+    public kullanici_degerlendirmesi(int kullanici_degerlendirme_id, int user_id, int film_id, int degerlendirme) {
+        this.kullanici_degerlendirme_id = kullanici_degerlendirme_id;
+        this.user_id = user_id;
+        this.film_id = film_id;
+        this.degerlendirme = degerlendirme;
+    }
+
+    public kullanici_degerlendirmesi(int user_id, int film_id, int degerlendirme) {
+        this.user_id = user_id;
+        this.film_id = film_id;
+        this.degerlendirme = degerlendirme;
+    }
+
+    public int getKullanici_degerlendirme_id() {
+        return kullanici_degerlendirme_id;
+    }
+
+    public void setKullanici_degerlendirme_id(int kullanici_degerlendirme_id) {
+        this.kullanici_degerlendirme_id = kullanici_degerlendirme_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
+
+    public int getDegerlendirme() {
+        return degerlendirme;
+    }
+
+    public void setDegerlendirme(int degerlendirme) {
+        this.degerlendirme = degerlendirme;
+    }
+    
+    
+}
